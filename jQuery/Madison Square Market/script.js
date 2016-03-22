2b70203b98523438fc75dfc01d4cc1828ccecbf6
Andre Sanchez <mail@asanchez.de>
2016-03-22 18:21:41 +0100

    Add Madison Square Market

--- jQuery/Madison Square Market/script.js
+++ jQuery/Madison Square Market/script.js
@@ -1,3 +1,13 @@
-/**
- * Created by andre on 22.03.2016.
- */
+var main = function () {
+    $(".dropdown").click(function () {
+        if ($('.dropdown-menu', this).css('display') == 'none') {
+            $(".dropdown-menu").fadeOut(200);
+            $('.dropdown-menu', this).slideDown();
+        }
+        else {
+            $('.dropdown-menu', this).slideUp();
+        }
+    });
+}
+
+$(document).ready(main);
