4790766289322721539c3b36141321c07153260b
Andre Sanchez <mail@asanchez.de>
2016-03-22 18:10:14 +0100

    Add Pocketbook

--- jQuery/Pocketbook/app.js
+++ jQuery/Pocketbook/app.js
@@ -1,3 +1,39 @@
-/**
- * Created by andre on 22.03.2016.
- */
+var main = function () {
+    $('form').submit(function () {
+        $('.first-name-error').html('');
+        $('.last-name-error').html('');
+        $('.email-error').html('');
+        $('.password-error').html('');
+
+        var firstName = $('#first').val();
+        if (firstName === "") {
+            $('.first-name-error').html('Please enter your first name.');
+        }
+
+        var lastName = $('#last').val();
+        if (lastName === "") {
+            $('.last-name-error').html('Please enter your last name.');
+        }
+
+        var email = $('#email').val();
+        if (email === "") {
+            $('.email-error').html('Please enter your email address.');
+        }
+        else if (email === 'xd@xd.de') {
+            $('.email-error').html('This email is already taken.');
+        }
+
+        var password = $('#password').val();
+        if (password === "") {
+            $('.password-error').html('Please enter a password.');
+        }
+        else if (password.length < 8) {
+            $('.password-error').html('Short passwords are easy to guess. Try one with at least 8 characters.');
+        }
+
+
+        return false;
+    })
+}
+
+$(document).ready(main);
