a1b9d7fb6302c6042c804d4a382214c6d7e9b7d4
Andre Sanchez <mail@asanchez.de>
2016-03-22 18:16:00 +0100

    Add Armando Perez

--- jQuery/Armando Perez/app.js
+++ jQuery/Armando Perez/app.js
@@ -1,3 +1,38 @@
-/**
- * Created by andre on 22.03.2016.
- */
+var main = function () {
+    $('.nav li').click(function () {
+        var category = $(this).attr('class');
+
+        if (category == 'nav-consumer') {
+            $('.thumbnail').removeClass('selected');
+            $('.consumer').addClass('selected');
+        }
+
+        if (category == 'nav-mobile') {
+            $('.thumbnail').removeClass('selected');
+            $('.mobile').addClass('selected');
+        }
+
+        if (category == 'nav-commerce') {
+            $('.thumbnail').removeClass('selected');
+            $('.commerce').addClass('selected');
+        }
+
+        if (category == 'nav-enterprise') {
+            $('.thumbnail').removeClass('selected');
+            $('.enterprise').addClass('selected');
+        }
+
+        if (category == 'nav-all') {
+            $('.thumbnail').removeClass('selected');
+            $('.all').addClass('selected');
+        }
+
+
+        $('.nav li').removeClass('active');
+        $(this).addClass('active');
+
+
+    });
+};
+
+$(document).ready(main);
