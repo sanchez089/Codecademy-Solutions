74c3f53c9a8414976e009681766c227446cade3d
Andre Sanchez <mail@asanchez.de>
2016-03-22 18:20:36 +0100

    Add Milford School

--- jQuery/Milford School/script.js
+++ jQuery/Milford School/script.js
@@ -1,3 +1,23 @@
-/**
- * Created by andre on 22.03.2016.
- */
+var main = function () {
+    $(".dropdown img").click(function () {
+        $(".dropdown-menu").slideToggle();
+    });
+    $(".main form").submit(function () {
+        $(".email-error").text("");
+        $(".password-error").text("");
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        if (email.length === 0) {
+            $(".email-error").text("Please enter your email.");
+        }
+        if (password.length === 0) {
+            $(".password-error").text("Please enter your password.");
+        }
+        return false;
+    });
+
+
+}
+
+$(document).ready(main);
